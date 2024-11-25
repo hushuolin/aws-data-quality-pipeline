a469e1fac69c2ac00765ac33fd825d284c5800af
Shuolin Hu <hu.shuo@northeastern.edu>
2024-11-25 13:41:57 -0500

    Update Glue Configurations to Craw the Schema of Bucket

--- deploy/glue_crawler.sh
+++ deploy/glue_crawler.sh
@@ -5,19 +5,26 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 # Parameters
-STACK_NAME="glue-crawler"
+STACK_NAME="GlueCatalogCrawler"
 TEMPLATE_FILE="cloudformation/glue_crawler.yaml"
-RAW_DATA_BUCKET_NAME="raw-transaction-data-bucket"
+SCHEMA_BUCKET_NAME="schema-detection-bucket-742465305217"
 GLUE_DATABASE_NAME="fintech_data_catalog"
-GLUE_CRAWLER_NAME="FintechDataCrawler"
-PROJECT_NAME="DataQualityPipeline"
+GLUE_CRAWLER_NAME="fintech_glue_crawler"
+PROJECT_NAME="fintech-transaction-data-quality-pipeline"
+
+# Dry run for template validation
+aws cloudformation validate-template \
+  --template-body file://$TEMPLATE_FILE || {
+  echo "Template validation failed."
+  exit 1
+}
 
 # Deploy the CloudFormation stack
 aws cloudformation deploy \
   --stack-name $STACK_NAME \
   --template-file $TEMPLATE_FILE \
   --parameter-overrides \
-    RawDataBucketName=$RAW_DATA_BUCKET_NAME \
+    SchemaDetectionBucketName=$SCHEMA_BUCKET_NAME \
     GlueDatabaseName=$GLUE_DATABASE_NAME \
     GlueCrawlerName=$GLUE_CRAWLER_NAME \
     ProjectName=$PROJECT_NAME \
